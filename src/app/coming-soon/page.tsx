f902e1859e2f77c7184d88bd7d2fb0b949c1fe3b
adah <joshuaadah6@gmail.com>
2024-01-23 12:59:36 +0100

    changes

--- src/app/coming-soon/page.tsx
+++ src/app/coming-soon/page.tsx
@@ -7,7 +7,7 @@ import { FaArrowRight } from 'react-icons/fa'
 
 function page() {
     return (
-        <div className='w-full text-white'>
+        <div className='w-full text-white font-syne'>
             <div className="h-screen bg-[#121820] flex items-center">
                 <MaxWidthWrapper className="  xl:w-[90%] ">
                     <Image
@@ -19,20 +19,30 @@ function page() {
                     />
                     <div className='flex justify-between items-start lg:items-center z-20 flex-col lg:flex-row '>
                         <div className='basis-2/4'>
-                            <h1 className='text-[64px]'>Coming <span className="text-[#f57c00]">Soon</span></h1>
+                            <h1 className='text-[64px] leading-[60px]'>Coming <span className="text-[#f57c00]">Soon</span></h1>
                             <p className='text-sm text-gray-400'>We are working on our website</p>
                         </div>
                         <div className='w-full basis-2/4 flex flex-col items-start' >
                             <div className="flex flex-col lg:flex-row mt-3 justify-between mb-5">
-                                <h4 className="">Join The <span className="text-[#f57c00]">ITSulu</span><br /> Experience</h4>
-                                <p className="">Lorem ipsum dolor sit amet, consectetuer <br />adipiscing elit sed diam nonummy.</p>
+                                <h4 className="font-bold text-lg my-2">Join The <span className="text-[#f57c00]">ITSulu</span><br /> Experience</h4>
+                                <p className=" text-gray-400 text-sm">Lorem ipsum dolor sit amet, consectetuer <br />adipiscing elit sed diam nonummy.</p>
                             </div>
 
-                            <div className="rounded-full bg-white flex items-center mb-7 relative w-full">
+                            <div className="rounded-full bg-white md:flex items-center mb-7 relative w-full hidden">
                                 <input type="text" placeholder="Your email address" className='px-[60px] h-[70px] text-nowrap text-center rounded-full' />
                                 <button type="submit" className="bg-[#f57c00] px-[60px] h-[70px] text-nowrap text-center absolute top-0 rounded-full right-0 border-[2px] border-white ">Subscribe Now</button>
                             </div>
-                            <a className="flex gap-2 items-center" href="/contact"><span>Contact Us</span>
+                            <a className=" gap-2 items-center hidden md:flex" href="/contact">
+                                <span>Contact Us</span>
+                                <FaArrowRight className='bg-[#f57c00] rounded-full w-6 h-6 p-1' />
+                            </a>
+                        </div>
+
+                        <div className='w-full flex flex-col my-4 space-y-2 md:hidden'>
+                            <input type="text" placeholder="Your email address" className='px-[60px] h-[70px] text-nowrap text-center rounded-full' />
+                            <button type="submit" className="bg-[#f57c00] px-[60px] h-[70px] text-nowrap text-center  rounded-full border-[2px] border-white ">Subscribe Now</button>
+                            <a className="flex gap-2 items-center mt-2" href="/contact">
+                                <span>Contact Us</span>
                                 <FaArrowRight className='bg-[#f57c00] rounded-full w-6 h-6 p-1' />
                             </a>
                         </div>
