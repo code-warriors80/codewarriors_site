dc18d95dbd3d3242e57caa6e28c9f3f9b72be296
codewarriors80 <muctarmohammed07@gmail.com>
2024-01-24 13:48:47 +0100

    update

--- src/app/coming-soon/page.tsx
+++ src/app/coming-soon/page.tsx
@@ -8,42 +8,40 @@ import { FaArrowRight } from 'react-icons/fa'
 function page() {
     return (
         <div className='w-full text-white font-syne'>
-            <div className="h-screen bg-[#121820] flex items-center">
-                <MaxWidthWrapper className="  xl:w-[90%] ">
-                    <Image
-                        src={"/20.jpg"}
-                        width={1020}
-                        height={1020}
-                        alt='Logo'
-                        className=' absolute bg-center filter grayscale-0 inset-0 object-cover w-full h-full -z-10'
-                    />
+            <img src='/30.jpg' className='w-full h-screen absolute z-0'/>
+            <div className="h-screen bg-[#121820] flex items-center relative z-10">
+                <MaxWidthWrapper className="">
                     <div className='flex justify-between items-start lg:items-center z-20 flex-col lg:flex-row '>
                         <div className='basis-2/4'>
-                            <h1 className='text-[64px] leading-[60px]'>Coming <span className="text-[#f57c00]">Soon</span></h1>
-                            <p className='text-sm text-gray-400'>We are working on our website</p>
+                            <h1 className='text-[60px] font-syne mb-5'>Coming <span className="text-[#f57c00]">Soon</span></h1>
+                            <p className='text-[15px] text-[#FFFFFF80] font-sora'>We are working on our website</p>
                         </div>
-                        <div className='w-full basis-2/4 flex flex-col items-start' >
-                            <div className="flex flex-col lg:flex-row mt-3 justify-between mb-5">
-                                <h4 className="font-bold text-lg my-2">Join The <span className="text-[#f57c00]">ITSulu</span><br /> Experience</h4>
-                                <p className=" text-gray-400 text-sm">Lorem ipsum dolor sit amet, consectetuer <br />adipiscing elit sed diam nonummy.</p>
+                        <div className='lg:w-[45.50%]  flex flex-col items-start justify-between' >
+                            <div className="flex  mt-3 items-center justify-between mb-5 w-full">
+                                <h4 className="font-syne text-[22px] my-2">Join The <span className="text-[#f57c00]">ITSulu</span><br /> Experience</h4>
+                                <p className=" text-[15px] text-[#FFFFFF80] font-sora">Lorem ipsum dolor sit amet, consectetuer <br />adipiscing elit sed diam nonummy.</p>
                             </div>
 
-                            <div className="rounded-full bg-white md:flex items-center mb-7 relative w-full hidden">
-                                <input type="text" placeholder="Your email address" className='px-[60px] h-[70px] text-nowrap text-center rounded-full' />
-                                <button type="submit" className="bg-[#f57c00] px-[60px] h-[70px] text-nowrap text-center absolute top-0 rounded-full right-0 border-[2px] border-white ">Subscribe Now</button>
+                            <div className="rounded-full bg-white items-center justify-between w-full p-1 md:flex hidden">
+                                <input type="text" placeholder="Your email address" className=' text-[#121820]  rounded-full px-5 outline-none' />
+                                <button type="submit" className="bg-[#f57c00] text-[#121820]  rounded-full px-10 py-5">Subscribe Now</button>
                             </div>
-                            <a className=" gap-2 items-center hidden md:flex" href="/contact">
+                            <a className=" gap-2 items-center hidden md:flex mt-10" href="/contact">
                                 <span>Contact Us</span>
-                                <FaArrowRight className='bg-[#f57c00] rounded-full w-6 h-6 p-1' />
+                                <div className='bg-[#f57c00] rounded-full  p-2'>
+                                    <FaArrowRight size={12}/>
+                                </div>  
                             </a>
                         </div>
 
                         <div className='w-full flex flex-col my-4 space-y-2 md:hidden'>
                             <input type="text" placeholder="Your email address" className='px-[60px] h-[70px] text-nowrap text-center rounded-full' />
                             <button type="submit" className="bg-[#f57c00] px-[60px] h-[70px] text-nowrap text-center  rounded-full border-[2px] border-white ">Subscribe Now</button>
-                            <a className="flex gap-2 items-center mt-2" href="/contact">
+                            <a className="flex gap-2 items-center mt-10" href="/contact">
                                 <span>Contact Us</span>
-                                <FaArrowRight className='bg-[#f57c00] rounded-full w-6 h-6 p-1' />
+                                <div className='bg-[#f57c00] rounded-full  p-2'>
+                                    <FaArrowRight size={12}/>
+                                </div>                
                             </a>
                         </div>
                     </div>
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,22 +6,24 @@ import MaxWidthWrapper from './components/MaxWidthWrapper'
 function notfound() {
     return (
         <div className="  font-sora block">
-            <div className='h-screen bg-[#121820] flex items-center'>
+            <img src='/map.png' className='w-full h-screen absolute z-0'/>
+            <div className='h-screen bg-[#121820] flex items-center relative z-10'>
                 <MaxWidthWrapper className=''>
                     <div className=' grid grid-cols-1 md:grid-cols-3 gap-5 text-white text-center '>
                         <div className=" items-center  block">
-                            <h5 className="font-semibold">Error</h5>
-                            <h1 className=" text-[100px] font-bold">404</h1>
-                            <h5 className="font-semibold block">Page <span className="text-[#f57c00]">not</span> found</h5>
+                            <h5 className="font-semibold text-[18px] font-syne">Error</h5>
+                            <h1 className=" text-[100px] font-sora">404</h1>
+                            <h5 className="text-[18px] font-syne block">Page <span className="text-[#f57c00]">not</span> found</h5>
                         </div>
                         <div className="flex flex-col ">
                             <div className="mt flex flex-col   items-center md:items-start">
-                                <p className=" text-gray-300 text-sm mb-4 block text-center md:text-start">The page you are looking for does not exist; <br />it may have been moved or removed altogether.</p>
+                                <p className=" text-[#FFFFFF80] text-[15px] mb-4 block text-center md:text-start">The page you are looking for does not exist; <br />it may have been moved or removed altogether.</p>
                                 <a className="border-[2px] border-[#f57c00] text-white px-10 py-3 rounded-full  inline-flex" href="/">
                                     <span>Back to homepage</span>
                                 </a>
                             </div>
                         </div>
+                        <img src='https://itsulu-react.netlify.app/_next/static/media/deco-2.8cba2eb2.svg' className='w-[220px]'/>
                     </div>
                 </MaxWidthWrapper>
             </div>
