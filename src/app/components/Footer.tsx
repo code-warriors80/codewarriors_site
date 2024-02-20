85597ba8a929d17b8e60b67311ba04807cff4624
Code~warrior200 <61137642+code-warrior200@users.noreply.github.com>
2024-02-19 23:25:23 -0800

    up

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -14,11 +14,19 @@ const footer = () => {
         <div className="sm:flex sm:items-center sm:justify-between">
             <span className="text-sm sm:text-center font-syne text-white">© 2023 <a href="https://flowbite.com/" className="hover:underline">Code warriors™</a>. All Rights Reserved.
             </span>
+            <div className=' flex'>
+                  <a href="#" className="text-white hover:text-gray-700">
+                    <p>Terms & Conditions</p>
+                  </a>
+                  <a href="#" className="text-white hover:text-gray-700">
+                    <p>FAQ</p>
+                  </a>
+              </div>
             <div className="flex mt-4 font-syne sm:justify-center sm:mt-0">
                 <a href="#" className="text-white hover:text-gray-700">
                     <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
-                          <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd"/>
-                      </svg>
+                      <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd"/>
+                    </svg>
                 </a>
                 <a href="#" className="text-white hover:text-gray-900 dark:hover:text-white ms-5">
                     <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 17">
