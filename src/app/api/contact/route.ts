50fc10f842226c396b9096a70129aa407f43ad86
adah <joshuaadah6@gmail.com>
2024-02-24 10:52:29 +0100

    changes

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -7,30 +7,40 @@ type RequestBody = {
   name: string;
 };
 
-export async function handler(
+export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
-  const { email, message, subject } = await request.body();
-  if (!email || !subject || !message) {
-    return response.status(400).json({
-      status: 400,
-      message: "Email and Message are required",
-    });
-  }
-  try {
-    const info = await transporter.sendMail({
-      ...mailOptions,
-      subject,
-      html: "",
-    });
+  if (request.method === "POST") {
+    const { email, message, subject } = await request.body();
 
-    console.log("Message sent: %s", info.messageId);
-    response.status(200).json({ message: "Email sent successfully!" });
-  } catch (error) {
-    return response.status(400).json({
-      status: 400,
-      message: "error.message",
-    });
+    if (!email || !subject || !message) {
+      return response.status(400).json({
+        status: 400,
+        message: "Email and Message are required",
+      });
+    }
+    try {
+      const info = await transporter.sendMail({
+        ...mailOptions,
+        subject,
+        html: "",
+      });
+
+      console.log("Message sent: %s", info.messageId);
+      response.status(200).json({ message: "Email sent successfully!" });
+    } catch (error) {
+      return response.status(400).json({
+        status: 400,
+        message: "error.message",
+      });
+    }
+  } else {
+    console.log(
+      "heyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"
+    );
+    response.setHeader("Allow", ["POST"]);
+    response.status(405).send(`Method ${request.method} Not Allowed`);
   }
 }
+
